9832f18f86df7d1059fe67e875f52db9e2217148
Fatih BAYTAR <matanist@hotmail.com>
2019-02-23 20:09:44 +0300

    Anasayfada slider eklendi.

--- RezerevasyonSistem/Controllers/DefaultController.cs
+++ RezerevasyonSistem/Controllers/DefaultController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RezervasyonSistem.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,26 @@ namespace RezerevasyonSistem.Controllers
 {
     public class DefaultController : Controller
     {
+        private RezervasyonSistemContext _context;
+        public DefaultController()
+        {
+            //Dependency Injection
+            _context = new RezervasyonSistemContext();
+        }
         // GET: Default
         public ActionResult Index()
         {
+            var odalar = _context.Odalar.ToList();
             ViewBag.Title = "Birşeyler";
-            return View();
+            return View(odalar);
+        }
+
+        public ActionResult RezervasyonYap(int? id)
+        {
+            if (id == null) return RedirectToAction("Index");
+            var seciliOda = _context.Odalar.FirstOrDefault(o => o.Id == id);
+            if (seciliOda == null) return RedirectToAction("Index");
+            return View(seciliOda);
         }
     }
 }
